4a319f157292097dcba896d7111025e1253d43b4
Gautam Arora <gautamsarora3@gmail.com>
2018-01-26 13:31:51 -0500

    Updating mock call checks

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -145,8 +145,8 @@ describe('helpers', () => {
       const data = { foo: 'bar' };
       const { firstChild } = parseXML('<a x="1" y="two">hello</a>');
       visitNode(firstChild, 0, converters, data);
-      expect(converters.a.mock.calls.length).toBe(1);
-      expect(converters.a.mock.calls[0][0]).toEqual({ x: '1', y: 'two' });
+      expect(converters.a).toHaveBeenCalledTimes(1);
+      expect(converters.a).toHaveBeenCalledWith({ x: '1', y: 'two' }, {"foo": "bar"});
     });
 
     it('should create a React element with type provided by the converter', () => {
